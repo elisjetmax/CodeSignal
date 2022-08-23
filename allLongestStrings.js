a1b22f2d19b87564b9f40186d2c3e293e428b731
elisjetmax <elisjetmax@users.noreply.github.com>
2022-08-23 15:08:39 -0500

    Given an array of strings, return another array containing all of its longest strings.

--- allLongestStrings.js
+++ allLongestStrings.js
@@ -1,11 +1,13 @@
 function solution(inputArray) {
   var arr = [];
   var max = 0;
+
   for (let a = 0; a < inputArray.length; a++) {
     let text = inputArray[a];
     arr.push(text.length);
     max = Math.max.apply(Math, arr);
   }
+
   const r = inputArray.filter((x) => x.length === max);
   return r;
 }

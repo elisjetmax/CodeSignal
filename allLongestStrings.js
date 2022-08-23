e6b35b3eb1ced2641fdabd2d0908a97654d174d8
elisjetmax <elisjetmax@users.noreply.github.com>
2022-08-23 15:07:56 -0500

    main

---
+++ allLongestStrings.js
@@ -0,0 +1,11 @@
+function solution(inputArray) {
+  var arr = [];
+  var max = 0;
+  for (let a = 0; a < inputArray.length; a++) {
+    let text = inputArray[a];
+    arr.push(text.length);
+    max = Math.max.apply(Math, arr);
+  }
+  const r = inputArray.filter((x) => x.length === max);
+  return r;
+}
